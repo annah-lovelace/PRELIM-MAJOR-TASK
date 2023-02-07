b320476541274b532a8b683cc03f6bcc13d94884
zarnnaih <98315886+zarnnaih@users.noreply.github.com>
2023-02-07 18:02:22 +0800

    Delete prog2/pregroup directory

--- prog2/pregroup/NoDenominatorException.java
+++
@@ -1,9 +0,0 @@
-package prog2.pregroup;
-
-public class NoDenominatorException extends RuntimeException {
-    public NoDenominatorException () {
-        System.out.println("NoDenominatorException occurred. Default fraction value 0/1 will be used");
-        System.out.println("You may still change it using the main menu");
-
-    }
-}
